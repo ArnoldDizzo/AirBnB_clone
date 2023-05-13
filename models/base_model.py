1ba07bd34b3028867ddd8f0774447a0934bd6caf
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-05-13 18:54:13 +0000

    update to BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
-        if len(kwargs) != 0:
+        if len(kwargs) > 0:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.fromisoformat(value)
