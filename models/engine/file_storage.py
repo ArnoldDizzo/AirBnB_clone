c55a3f9f2969f242ecc5b6d67698370669a29b1e
Arnold Emmanuel Mukama <mukamaarnold24@gmail.com>
2023-05-14 16:12:20 -0700

    Added some files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -34,7 +34,7 @@ class FileStorage:
             json.dump(object_dict, f)
 
     def reload(self):
-        """_Deserialize from file.json"""
+        """to deserialize from file.json"""
         try:
             with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
                 dicti = json.load(f)
