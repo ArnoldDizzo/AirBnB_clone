4e9b6761b455b1c72c1902769f2aaa7334aee65c
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-05-14 16:21:00 +0000

    tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -13,6 +13,15 @@ class TestBaseModel_insatiation(unittest.TestCase):
     def test_created_obj_stored(self):
         self.assertIn(BaseModel(), storage.all().values())
 
+    def test_id_is_str(self):
+        self.assertEqual(str, type(BaseModel().id))
+
+    def test_created_at_is_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().created_at))
+
+    def test_updated_at_is_str(self):
+        self.assertEqual(datetime, type(BaseModel().updated_at))
+
 
 
 if __name__ == "__main__":
